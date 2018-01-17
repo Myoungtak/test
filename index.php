bd0bc550dd6a42c1e45a7eb75108e1fce1e8d03b
Myoungtak <mthann@naver.com>
2018-01-17 23:18:58 +0900

    first version

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> 정법검색 도우미</title>
+  </head>
+  <body>
+
+    <nav>
+      <ol>
+        <li>  <a href="input.html" target="_blank" title="input.html ">  입력 </a> </li>
+        <li>  <a href="motify.html">  수정 </a> </li>
+        <li>  <a href="delete.html">  삭제 </a> </li>
+        <li>  <a href="searchByNum.html">  검색-번호 </a> </li>
+        <li>  <a href="searchByWord.html">  검색-검색어 </a> </li>
+        <li>  <a href="inventory.html">  목록 </a> </li>
+      </ol>
+    </nav>
+
+
+    <article class="">
+
+
+
+
+
+      <h1> 입력 </h1>
+      번호,
+      질문,
+      법문
+      <p>
+        Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript it forms a triad of cornerstone technologies for the World Wide Web.[3] Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+        <img src="coding01.jpeg" width="50%" alt="">
+      </p>
+
+      <p>
+      HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets. Tags such as  and introduce content into the page.
+      </p>
+
+
+    </article>
+
+  </body>
+</html>
